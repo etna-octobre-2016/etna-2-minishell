ebb568adf5a85f7b02b5ced13a9ef733f0abec92
Steven Leclerc <stevenleclerc@gmail.com>
2015-05-11 17:56:57 +0200

    Adding function - strchar
    Adding my_strchar
    It allows you to search char on string
    return 0 if found / 1 not found

--- src/string.c
+++ src/string.c
@@ -61,3 +61,15 @@ char    *my_strstr(char *str, char *to_find)
       }
   return (0);
 }
+
+int my_strchar(char to_find, char* string)
+{
+  int i;
+
+  for(i = 0; string[i] != '\0'; i++)
+  {
+    if (string[i] == to_find)
+      return (0);
+  }
+  return (1);
+}
