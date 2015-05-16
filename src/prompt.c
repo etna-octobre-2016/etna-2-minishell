d11cbe3a191d5279e3151d13bc09f5ac1f0788a4
Itadakimas <said.ahemt@gmail.com>
2015-05-16 12:51:22 +0200

    prepares prompt for command splitting
    - adds s_cmd_list structure
    - adds t_cmd_list type
    - adds prompt_split_cmd function declaration

--- src/prompt.c
+++ src/prompt.c
@@ -1,31 +1,43 @@
-#include <stdbool.h>
 #include "../lib/my/src/headers/my.h"
 #include "headers/parser.h"
 #include "headers/prompt.h"
 
-void    prompt_init()
+#include <stdio.h>
+
+void          prompt_init()
 {
-  bool  is_running;
-  char  *cmd;
+  bool        is_running;
+  char        *cmd;
+  t_cmd_list  *cmd_list;
 
   is_running = true;
   while (is_running)
   {
     prompt_show();
     cmd = prompt_read_cmd();
-    if (cmd != NULL && my_strlen(cmd) > 0)
-    {
-      parser(cmd);
-    }
+    cmd_list = prompt_split_cmd(cmd);
+    printf("addr cmd_list %p\n", cmd_list);
+    // if (cmd != NULL && my_strlen(cmd) > 0)
+    // {
+    //   parser(cmd);
+    // }
   }
 }
 
 void prompt_show()
 {
-  my_printf("%s", PROMPT_DEFAULT_STRING);
+  my_putstr(PROMPT_DEFAULT_STRING);
 }
 
 char *prompt_read_cmd()
 {
   return my_readline(PROMPT_BUFFER_SIZE);
+}
+
+t_cmd_list *prompt_split_cmd(char *cmd)
+{
+  printf("%s\n", cmd);
+
+
+  return (NULL);
 }
