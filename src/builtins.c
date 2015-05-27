f2bb0fc330d56aff04d51af386c29fc7fa49d6a8
Saïd AHEMT <said.ahemt@nolaroads.com>
2015-05-27 13:51:04 +0200

    adds builtin functions prototypes

---
+++ src/builtins.c
@@ -0,0 +1,44 @@
+#include "headers/builtins.h"
+
+int   builtin_cd(char **commandSplit)
+{
+  int ret;
+
+  ret = 0;
+  return (ret);
+}
+int   builtin_env(char **commandSplit)
+{
+  int ret;
+
+  ret = 0;
+  return (ret);
+}
+int   builtin_handle(char **commandSplit)
+{
+  int ret;
+
+  ret = 0;
+  return (ret);
+}
+int   builtin_path(char **commandSplit)
+{
+  int ret;
+
+  ret = 0;
+  return (ret);
+}
+int   builtin_setenv(char **commandSplit)
+{
+  int ret;
+
+  ret = 0;
+  return (ret);
+}
+int   builtin_unsetenv(char **commandSplit)
+{
+  int ret;
+
+  ret = 0;
+  return (ret);
+}
