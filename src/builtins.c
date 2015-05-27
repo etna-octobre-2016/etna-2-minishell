6ccec190391fc25d0675208cdc5bcb9c3d6160cf
Itadakimas <said.ahemt@gmail.com>
2015-05-27 23:54:31 +0200

    adds builtin commands detection in the builtin_handler function

--- src/builtins.c
+++ src/builtins.c
@@ -1,10 +1,13 @@
 #include "headers/builtins.h"
 
+#include <stdio.h> // @todo: remove
+
 int   builtin_cd(char **commandSplit)
 {
   int ret;
 
   ret = 0;
+  printf("builtin = cd, cmd = %s\n", commandSplit[0]);
   return (ret);
 }
 int   builtin_env(char **commandSplit)
@@ -12,13 +15,31 @@ int   builtin_env(char **commandSplit)
   int ret;
 
   ret = 0;
+  printf("builtin = env, cmd = %s\n", commandSplit[0]);
   return (ret);
 }
-int   builtin_handle(char **commandSplit)
+int         builtin_handle(char **commandSplit)
 {
-  int ret;
+  int       i;
+  int       ret;
+  t_builtin builtins[] = {
+    {"cd", builtin_cd},
+    {"env", builtin_env},
+    {"path", builtin_path},
+    {"setenv", builtin_setenv},
+    {"unsetenv", builtin_unsetenv},
+    {NULL,NULL}
+  };
 
-  ret = 0;
+  ret = BUILTIN_UNKNOWN;
+  for (i = 0; builtins[i].name != NULL; i++)
+  {
+    if (my_strcmp(builtins[i].name, commandSplit[0]) == 0)
+    {
+      ret = builtins[i].func(commandSplit);
+      break;
+    }
+  }
   return (ret);
 }
 int   builtin_path(char **commandSplit)
@@ -26,6 +47,7 @@ int   builtin_path(char **commandSplit)
   int ret;
 
   ret = 0;
+  printf("builtin = path, cmd = %s\n", commandSplit[0]);
   return (ret);
 }
 int   builtin_setenv(char **commandSplit)
@@ -33,6 +55,7 @@ int   builtin_setenv(char **commandSplit)
   int ret;
 
   ret = 0;
+  printf("builtin = setenv, cmd = %s\n", commandSplit[0]);
   return (ret);
 }
 int   builtin_unsetenv(char **commandSplit)
@@ -40,5 +63,6 @@ int   builtin_unsetenv(char **commandSplit)
   int ret;
 
   ret = 0;
+  printf("builtin = unsetenv, cmd = %s\n", commandSplit[0]);
   return (ret);
 }
